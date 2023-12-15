18683aabd226a373ae847e0a235f7de5a00d8354
DowonKim <kimdowon1404@gmail.com>
2023-12-15 15:24:13 +0900

    feat : 기본 markdown생성

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -15,15 +15,22 @@ const PostPage = async ({ params: { slug } }: IProps) => {
     const { title, description, date, category, path, content } = await getPostData(slug);
 
     return (
-        <article>
-            <Image src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
-            <section>
-                <div>
+        <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+            <Image
+                className="w-full h-1/5 max-h-[500px]"
+                src={`/images/posts/${path}.png`}
+                alt={title}
+                width={760}
+                height={420}
+            />
+            <section className="flex flex-col p-4">
+                <div className="flex items-center self-end text-sky-600">
                     <AiTwotoneCalendar />
-                    <p>{date.toString()}</p>
+                    <p className="font-semibold ml-2">{date.toString()}</p>
                 </div>
-                <h1>{title}</h1>
-
+                <h1 className="text-4xl font-bold mb-2">{title}</h1>
+                <p className="text-xl font-bold">{description}</p>
+                <div className="w-44 border-2 border-sky-600 mt-4 mb-8"></div>
                 <MarkdownViewer content={content} />
             </section>
         </article>
